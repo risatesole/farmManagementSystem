f017cc94b21e43b639882fe9169fb6d3ac0e9adc
Henry Edilio Ramírez C. <henryedilio@gmail.com>
2025-06-16 21:18:39 +0000

    refactor: remove unnecessary comments

--- platform/frontend/src/App.tsx
+++ platform/frontend/src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 
-// If using React 17+, you can remove the explicit React import above and ensure tsconfig.json has "jsx": "react-jsx"
-
 const HomePage: React.FC = () => (
   <>
     <Navbar />
